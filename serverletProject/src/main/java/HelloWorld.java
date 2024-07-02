3d8b08cf371d15f5f5ade2c783b87c83967393d7
Nikesh Adhikari <nikeshad254@gmail.com>
2024-07-02 07:21:23 +0545

    tomcat servlet made and running with another page /hello

---
+++ serverletProject/src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ serverletProject/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="/serverletProject/hello">Hello World!</a>
+</body>
+</html>
