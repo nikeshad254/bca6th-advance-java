f3d6bc826068744dd84e01d4418a38da637f0918
Nikesh Adhikari <nikeshad254@gmail.com>
2024-07-03 07:19:59 +0545

    init and destroy used in hello world class

--- serverletProject/src/main/java/HelloWorld.java
+++ serverletProject/src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +9,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class HelloWorld extends HttpServlet {
+
+    String message;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        message = "Hello World!";
+    }
+
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
@@ -16,8 +26,14 @@ public class HelloWorld extends HttpServlet {
         out.println("<title>Hello World</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Hello World</h1>");
+        out.println("<h1>"+message+"</h1>");
         out.println("</body>");
         out.println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        message="";
     }
 }
